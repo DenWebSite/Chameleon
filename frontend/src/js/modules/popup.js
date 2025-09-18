cbf1d6a7f28e5a4b8fe1f4377ca3f39a6ec66960
nikolai-dev82 <nikolaikoval4uk@yandex.ru>
2025-09-18 10:05:57 +0300

    Изменил адрес API

--- frontend/src/js/modules/popup.js
+++ frontend/src/js/modules/popup.js
@@ -128,7 +128,7 @@ export default function initToggleModal() {
       };
 
       // Отправляем на сервер
-      const response = await fetch("https://ваш-сервер.com/api/contact", {
+      const response = await fetch("http://127.0.0.1:5000/api/contact", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
