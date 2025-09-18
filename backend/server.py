ee1c95281d8a3f16410cd965cffb79220c8b0fb4
nikolai-dev82 <nikolaikoval4uk@yandex.ru>
2025-09-18 17:32:23 +0300

    Добавил CORS

--- backend/server.py
+++ backend/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from config import *
 
 import sqlite3 
@@ -10,6 +11,7 @@ from datetime import datetime
 
 
 app = Flask(__name__)
+CORS(app)
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
